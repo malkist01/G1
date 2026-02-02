03e02944dd1ab7ac63cbd53a3f68096fbb500a90
malkist01 <164641045+malkist01@users.noreply.github.com>
2026-02-02 11:47:32 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,10 +7,10 @@ rm -f clang.tar.gz
 git clone https://github.com/sohamxda7/llvm-stable -b gcc64 --depth=1 gcc
 git clone https://github.com/sohamxda7/llvm-stable -b gcc32  --depth=1 gcc32
 git clone --depth=1 https://github.com/malkist01/AnyKernel2 AnyKernel
-chmod +x ginkgo.sh && patch -p1 -F 3 < seccomp.patch
-chmod +x hooks.patch && patch -p1 -F 3 < hooks.patch
+chmod +x ginkgo.sh && git am seccomp.patch
+chmod +x hooks.patch && git am hooks.patch
 curl https://raw.githubusercontent.com/malkist01/KernelSU-Next/refs/heads/legacy/kernel/setup.sh | bash
-chmod +x susfs-2.0.0.patch && patch -p1 -F 3 < susfs-2.0.0.patch
+chmod +x susfs-2.0.0.patch && git am susfs-2.0.0.patch
 echo "Done"
 ZIPNAME="Teletubies"
 DEVICE="ginkgo"
